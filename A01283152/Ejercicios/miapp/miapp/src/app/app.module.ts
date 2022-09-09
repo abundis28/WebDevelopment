7e035ba665d5e641199c3ec27428b18e1bb70114
Andrés Abundis <andresabundis@gmail.com>
2022-09-09 18:38:31 -0500

    Modifies import in main app template to enable the use of NgModel.

--- A01283152/Ejercicios/miapp/miapp/src/app/app.module.ts
+++ A01283152/Ejercicios/miapp/miapp/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -22,7 +23,9 @@ import { NgforComponent } from './components/ngfor/ngfor.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
